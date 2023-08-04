566f669ef27d83327a70b6d277cfbdc00a40aea7
M0mahmoud <houdmohamed85@gmail.com>
2023-08-04 19:06:22 +0300

    Components Of Layouts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,7 @@
-//app/page.tsx
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <>
+      <h1>Home</h1>
+    </>
   );
 }
