d8894d4962b5b391e9184970ced1e19628bd3694
M0mahmoud <houdmohamed85@gmail.com>
2023-08-13 00:18:22 +0300

    Thrad Card

--- actions/thread.actions.ts
+++ actions/thread.actions.ts
@@ -3,7 +3,6 @@ import { connectDB } from "@/lib/mongoose";
 import Thread from "@/models/Thread";
 import User from "@/models/User";
 import { revalidatePath } from "next/cache";
-
 interface ThreadProps {
   text: string;
   author: string;
@@ -31,5 +30,30 @@ export async function createThread({
     revalidatePath(path);
   } catch (err) {
     console.log("Error Creating new thread", err);
+  }
+}
+
+export async function fetchPosts({ pageNumber = 1, peerPage = 20 }) {
+  try {
+    connectDB();
+    const skipAmount = (pageNumber - 1) * peerPage;
+
+    const thradsCount = await Thread.find({
+      parentId: { $in: [null, undefined] },
+    }).countDocuments();
+
+    const threads = await Thread.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: -1 })
+      .skip(skipAmount)
+      .limit(peerPage)
+      .populate("author");
+
+    const isNext = thradsCount > skipAmount + threads.length;
+    return {
+      threads,
+      isNext,
+    };
+  } catch (err) {
+    console.log("err:", err);
   }
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,43 @@
-export default function Home() {
+import { fetchPosts } from "@/actions/thread.actions";
+import ThreadCard from "@/components/cards/ThreadCard";
+import { currentUser } from "@clerk/nextjs";
+
+export default async function Home() {
+  const user = await currentUser();
+  if (!user) return null;
+  const threads = await fetchPosts({ pageNumber: 1, peerPage: 10 });
+
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {threads?.threads.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {threads?.threads.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+
+      {/* <Pagination
+        path='/'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      /> */}
     </>
   );
 }
