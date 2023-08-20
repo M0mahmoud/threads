bb52061f5141e0a14eeefb31287f3a36f8820e98
M0mahmoud <houdmohamed85@gmail.com>
2023-08-20 23:04:17 +0300

    Adding Search Page

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -1,6 +1,7 @@
 "use server";
 import Thread from "@/models/Thread";
 import User from "@/models/User";
+import { FilterQuery, SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
 import { connectDB } from "../lib/mongoose";
 
@@ -56,6 +57,48 @@ export async function fetchUser(userId: string) {
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
   }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = 1,
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const regex = new RegExp(searchString, "i");
+    const query: FilterQuery<typeof User> = { id: { $ne: userId } };
+
+    if (searchString !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error) {}
 }
 
 export async function fetchUserThreads(userId: string) {
---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,58 @@
+import { fetchUser, fetchUsers } from "@/actions/user.action";
+import UserCard from "@/components/cards/UserCard";
+import Pagination from "@/components/shared/Pagination";
+import Searchbar from "@/components/shared/Searchbar";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const Search = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 20,
+  })!;
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+      <Searchbar routeType="search" />
+      <div className="mt-14 flex flex-col gap-9">
+        {result?.users?.length === 0 ? (
+          <p className="no-result">No Result</p>
+        ) : (
+          <>
+            {result?.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result?.isNext!}
+      />
+    </section>
+  );
+};
+
+export default Search;
